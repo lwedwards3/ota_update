7f2d0a9a3ea4e56dc073cf1964b294064eda1951
lwedwards3 <lwedwards3@gmail.com>
2021-01-06 15:37:53 -0500

    json secrets file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import json
 
+with open("app/secrets.json", 'r') as source:
+    s = source.read()
+    secrets = json.loads(s)
 
 def connectToWifiAndUpdate():
-    import time, machine, network, gc, app.secrets as secrets
+    import time, machine, network, gc#, app.secrets as secrets
     time.sleep(1)
     print('Memory free', gc.mem_free())
 
@@ -11,11 +15,11 @@ def connectToWifiAndUpdate():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        sta_if.connect(secrets['WIFI_SSID'], secrets['WIFI_PASSWORD'])
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-    otaUpdater = OTAUpdater(secrets.GIT_URL, main_dir='app', secrets_file="secrets.py")
+    otaUpdater = OTAUpdater(secrets['GIT_URL'], main_dir='app', secrets_file="secrets.json")
     hasUpdated = otaUpdater.install_update_if_available()
     if hasUpdated:
         machine.reset()
